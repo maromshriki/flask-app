4c63501b9ca496bbd4adb54f042b72945ffa8947
maromshriki <marom.shriki@gmail.com>
2026-04-08 13:51:00 +0300

    ci

--- app/app.py
+++ app/app.py
@@ -30,7 +30,7 @@ def get_db_version():
 @app.route("/")
 def home():
     db_version = get_db_version()
-    return f"<h1>🚀 Flask Production-like App</h1><p>Postgres version: {db_version}</p>"
+    return f"<h1>🚀 Flask Production-like Apps</h1><p>Postgres version: {db_version}</p>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
